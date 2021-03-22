16d8fab9f378420f0667255ec1237e48e1581782
Naima <nbensassi@hotmail.com>
2021-03-21 22:23:42 -0400

    create form react

---
+++ practice/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+import NavBar from "./component/NavBar"
+import Form from "./component/Form"
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ practice/src/component/NavBar/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
